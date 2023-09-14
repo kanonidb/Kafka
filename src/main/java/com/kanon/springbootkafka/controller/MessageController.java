e6f4c003a7202475ac3ec98a00e573c2c9d4ede2
kanonidb <kanonidb@gmail.com>
2023-09-14 18:01:46 +0600

    first commit

---
+++ src/main/java/com/kanon/springbootkafka/controller/MessageController.java
@@ -0,0 +1,32 @@
+package com.kanon.springbootkafka.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kanon.springbootkafka.kafka.KafkaProducer;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+	
+	@Autowired
+	private KafkaProducer kafkaProducer;
+	
+	@GetMapping("/publish")
+	public ResponseEntity<String>publish(@RequestParam("message") String message){
+		
+		kafkaProducer.sendMessage(message);
+		return ResponseEntity.ok("Message sent to the topic");
+	}
+
+
+
+}
---
+++ src/main/java/com/kanon/springbootkafka/kafka/KafkaConsumer.java
@@ -0,0 +1,5 @@
+package com.kanon.springbootkafka.kafka;
+
+public class KafkaConsumer {
+
+}
